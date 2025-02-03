89fdc41d476207fdfd0712c09a5b9d0dba093f73
Nagi-ei <nakt4453@gmail.com>
2025-02-03 16:23:19 +0900

    ♻️ Add regex for email and password

--- src/components/ValidationRow.jsx
+++ src/components/ValidationRow.jsx
@@ -13,18 +13,32 @@ export default function ValidationRow({
 }) {
   const [err, setErr] = useState(null);
 
+  const emailRegex = /[\w\-\.]+\@[\w\-\.]+/;
+  // [\w] : 단어 문자(word character) - 알파벳, 숫자, 언더스코어(_)를 의미
+  // 1. [\w\-\.]+ : 이메일 아이디 부분 - 알파벳,숫자,언더스코어,하이픈,점이 1회 이상
+  // 2. \@ : @ 기호
+  // 3. [\w\-\.]+ : 도메인 부분 - 알파벳,숫자,언더스코어,하이픈,점이 1회 이상
+
+  const passwordRegex = /^(?=.*\d)[a-zA-Z\d]{8,}$/;
+  // ^ : 문자열의 시작
+  // (?=.*\d) : 전방 탐색(lookahead), .* 는 모든 문자(0개 이상), \d는 숫자를 의미
+  //            -> 뒤에 숫자가 최소 1개 이상 있어야 함
+  // [a-zA-Z\d] : a-z, A-Z, 숫자(0-9)만 허용
+  // {8,} : 8자리 이상
+  // $ : 문자열의 끝
+
   const isValid = () => {
     switch (name) {
       case 'Email':
-        if (!data.includes('@') || !data.includes('.')) {
+        if (!emailRegex.test(data)) {
           setErr('Email is not valid');
         } else {
           setErr(null);
         }
         break;
       case 'Password':
-        if (data.length < 8) {
-          setErr('Password must be at least 8 characters');
+        if (!passwordRegex.test(data)) {
+          setErr('Password is not valid');
         } else {
           setErr(null);
         }
